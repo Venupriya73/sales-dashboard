f2fa638cb262639d63d6b064137205eb65ee0802
venupriya <venupriyaramaraj@gmail.com>
2026-06-11 11:09:47 +0530

    feat: update CORS for production

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,7 +6,19 @@ const transactionRoutes = require('./routes/transactions');
 
 const app = express();
 
-app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(cors({
+  origin: function(origin, callback) {
+    const allowedOrigins = [
+      'http://localhost:3000',
+      'https://sales-dashboard-venupriya.vercel.app'
+    ];
+    if (!origin || allowedOrigins.indexOf(origin) !== -1 || origin.endsWith('.vercel.app')) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+}));
 app.use(express.json());
 
 app.use('/api/transactions', transactionRoutes);
